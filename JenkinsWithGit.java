b579e73eb272e1528ec66bcdc24632b8597392c2
Hadi24qahtani <124071469+Hadi24qahtani@users.noreply.github.com>
2023-02-12 11:34:12 +0300

    Add files via upload

---
+++ JenkinsWithGit.java
@@ -0,0 +1,14 @@
+
+
+public class JenkinsWithGit {
+
+    public static void public static void main(String[] args) {
+        
+        int charPerLine = 0;
+        for (int i =33; i<256; i++, charPerLine++)
+    }
+
+    system.out.print((char)i+((charPerLine%10==0)?"\n":"\t"));
+    system.out.print();
+    
+}
